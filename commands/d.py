ca32c2d9f93eefdb5ff7e99dd2ce9399f3798ca2
Craeckie <josia-internet@rosskopfs.de>
2016-05-23 19:13:34 +0200

    Using interactive call

--- commands/d.py
+++ commands/d.py
@@ -5,16 +5,18 @@ def func(action):
     directory = action['directory']
     relative = action['relative']
 
-    p = subprocess.Popen(["docker-compose", "down"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = subprocess.call(["docker-compose", "down"])
+    # p = subprocess.Popen(["docker-compose", "down"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    out, err = p.communicate()
+    # out, err = p.communicate()
 
-    if out != "":
-        print(out.decode("UTF-8"))
+    # if out != "":
+    #     print(out.decode("UTF-8"))
 
-    print("Error removing " + relative + "!")
+    if not res == 0:
+        print("Error removing " + relative + "!")
     
-    print(bcolors.FAIL + err.decode("UTF-8") + bcolors.ENDC)
+    # print(bcolors.FAIL + err.decode("UTF-8") + bcolors.ENDC)
 
     return 0
 
