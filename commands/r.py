00446be068740795f5a5f04c22c98f0840a3807c
Craeckie <josia-internet@rosskopfs.de>
2016-05-23 19:19:13 +0200

    Changed to subprocess.call for interactivity

--- commands/r.py
+++ commands/r.py
@@ -5,19 +5,20 @@ def func(action):
     directory = action['directory']
     relative = action['relative']
 
-    p = subprocess.Popen(["docker-compose", "rm", "-f"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = subprocess.call(["docker-compose", "rm", "-f"])
+    # p = subprocess.Popen(["docker-compose", "rm", "-f"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    out, err = p.communicate()
+    # out, err = p.communicate()
 
-    if out != "":
-        print(out.decode("UTF-8"))
+     #if out != "":
+     #    print(out.decode("UTF-8"))
 
-    if p.returncode == 0:
-        print('Removed ' + relative)
-    else:
+    if res != 0:
         print("Error removing " + relative + "!")
+    #     print('Removed ' + relative)
+    # else:
     
-    print(bcolors.FAIL + err.decode("UTF-8") + bcolors.ENDC)
+     #print(bcolors.FAIL + err.decode("UTF-8") + bcolors.ENDC)
 
     return 0
 
--- commands/t.py
+++ commands/t.py
@@ -5,19 +5,18 @@ def func(action):
     directory = action['directory']
     relative = action['relative']
 
-    p = subprocess.Popen(["docker-compose", "stop"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = subprocess.call(["docker-compose", "stop"])
+    # p = subprocess.Popen(["docker-compose", "stop"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    out, err = p.communicate()
+     #out, err = p.communicate()
 
-    if out != "":
-        print(out.decode("UTF-8"))
+     #if out != "":
+     #    print(out.decode("UTF-8"))
 
-    if p.returncode == 0:
-        print('Stopped ' + relative)
-    else:
+    if res != 0:
         print("Error stopping " + relative + "!")
     
-    print(bcolors.FAIL + err.decode("UTF-8") + bcolors.ENDC)
+    # print(bcolors.FAIL + err.decode("UTF-8") + bcolors.ENDC)
 
     return 0
 
