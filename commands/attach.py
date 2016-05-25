ecd213d7725b631d66967346340c864a433578ed
Craeckie <josia-internet@rosskopfs.de>
2016-05-25 14:46:04 +0200

    Renamed commands to more intuitive names

--- commands/attach.py
+++ commands/attach.py
@@ -27,6 +27,28 @@ def func(action):
       elif len(containers) > 1:
         # ask..
         print("Choose container:")
+        valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+        if default is None:
+            prompt = " [y/n] "
+        elif default == "yes":
+            prompt = " [Y/n] "
+        elif default == "no":
+            prompt = " [y/N] "
+        else:
+            raise ValueError("invalid default answer: '%s'" % default)
+
+        while True:
+            sys.stdout.write(question + prompt)
+            choice = raw_input().lower()
+            if default is not None and choice == '':
+                return valid[default]
+            elif choice in valid:
+                return valid[choice]
+            else:
+                sys.stdout.write("Please respond with 'yes' or 'no' "
+                                 "(or 'y' or 'n').\n")
+                                 
         index = 1
       container_id = containers[index]
       print("Attaching to " + container_id)
