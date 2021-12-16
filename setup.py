40810027120e1aa1b7a8c46ef859312a57eadc6d
Josia <josia-login@rosskopfs.de>
2021-12-16 17:43:46 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='portmgr',
-    version='1.3.1dev0',
+    version='1.3.2',
     url="https://github.com/Craeckie/portmgr",
     description="Simple command interface to manage multiple Docker container",
     packages=find_packages(), #['portmgr'],
