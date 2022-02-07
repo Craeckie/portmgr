5b01c2073c9cb7b9ebc90a7a9656e524a99efd7a
Josia <josia-login@rosskopfs.de>
2022-02-07 15:33:54 +0100

    fix column alignments without network stats

--- portmgr/commands/stats.py
+++ portmgr/commands/stats.py
@@ -21,6 +21,7 @@ def func(action):
         key=attrgetter('name'))
 
     values = []
+    has_network_stats = False
     for container in containers:
         stats = project.client.stats(container.name, stream=False)
         memory = stats["memory_stats"]
@@ -31,6 +32,7 @@ def func(action):
             received = format_size(sum(stats['rx_bytes'] for iface, stats in network.items()))
             sent = format_size(sum(stats['tx_bytes'] for iface, stats in network.items()))
             columns = (container.service, usage, limit, received, sent)
+            has_network_stats = True
         else:
             columns = (container.service, usage, limit)
         values.append(columns)
@@ -39,7 +41,8 @@ def func(action):
         values = sorted(values, key=cmp_to_key(lambda s1, s2: parse_size(s2[1]) - parse_size(s1[1])))
         print(tabulate(values,
                        headers=['Service', 'Mem Usage', 'Mem Limit', 'Net Recv', 'Net Sent'],
-                       colalign=['left', 'right', 'right', 'right', 'right']))
+                       colalign=['left', 'right', 'right', 'right', 'right'] if has_network_stats
+                           else ['left', 'right', 'right']))
         print('')
 
     return 0
