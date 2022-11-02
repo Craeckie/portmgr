cc5cc5002295c8c05230675a00f5afa6240f56b9
dev <josia-login@rosskopfs.de>
2022-11-02 19:15:08 +0100

    updated to 1.4.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='portmgr',
-    version='1.4.6dev0',
+    version='1.4.7',
     url="https://github.com/Craeckie/portmgr",
     description="Simple command interface to manage multiple Docker container",
     packages=find_packages(), #['portmgr'],
