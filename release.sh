fc7ad225fffd476588dc6f9512e0555c46a9aee5
ultrabook <5006444+Craeckie@users.noreply.github.com>
2024-06-18 22:02:05 +0200

    improved release scripts

--- release.sh
+++ release.sh
@@ -1,1 +1,7 @@
-python setup.py sdist && twine upload dist/* && curl -X PURGE https://pypi.org/project/portmgr; rm -rf build/ dist/ portmgr.egg-info/
+#!/bin/bash
+set -x
+set -e
+python setup.py sdist
+twine upload dist/*
+curl -X PURGE https://pypi.org/project/portmgr
+rm -rf build/ dist/ portmgr.egg-info/
