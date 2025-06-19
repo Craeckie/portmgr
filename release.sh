f19eb7e9a836e713a8b537157f8dfd99a0575a5e
xiaomi <xiaomi@example.com>
2025-06-19 14:14:26 +0200

    chore: use uv to build&publish

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 rm -rf dist/ portmgr.egg-info/
-python setup.py sdist
-twine upload dist/*
-curl -X PURGE https://pypi.org/project/portmgr
+uv build
+uv publish
+#curl -X PURGE https://pypi.org/project/portmgr
 rm -rf build/ dist/ portmgr.egg-info/
