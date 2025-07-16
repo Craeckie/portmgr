b6ef59ab132fd048b9dbf93eaf2ec7fb8aca4a9d
xiaomi <xiaomi@example.com>
2025-07-16 11:01:26 +0200

    fix: error message

--- src/portmgr/commands/push.py
+++ src/portmgr/commands/push.py
@@ -53,8 +53,9 @@ def func(action):
                              service
                              ]
                             )
-                print(f"Error pushing {service}!")
-            except subprpcess.TimeoutExpired:
+                    if new_res != 0:
+                        print(f"Error pushing {service}!")
+            except subprocess.TimeoutExpired:
                 print(f"Error building {service}! Build timed out.")
             if new_res != 1:
                 res = new_res
